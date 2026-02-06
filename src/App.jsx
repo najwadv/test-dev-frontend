23599657d1be007bc7c713d7b085874075213226
Najwa Diva <najwaadiva.0302@gmail.com>
2026-02-06 15:43:27 +0700

    init: setup React project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Technical Test - Frontend Developer</h1>
+        <h3>PT. Integrasia Utama</h3>
+        <p>Nama: Najwa Diva Azzahra | Tanggal pengerjaan: 6-9 Februari 2026</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
