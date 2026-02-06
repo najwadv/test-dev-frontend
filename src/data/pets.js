834e0807c4dc48261cd1c266b5222ea10a70cd84
Najwa Diva <najwaadiva.0302@gmail.com>
2026-02-06 17:03:11 +0700

    feat: Add pets data array structure

---
+++ src/data/pets.js
@@ -0,0 +1,53 @@
+const pets = [
+  {
+    species: "Dog",
+    name: "Otto",
+    breed: "Golden Retriever",
+    characteristic: "Energetic and loves to play ball",
+    isFavorite: true,
+  },
+  {
+    species: "Dog",
+    name: "Max",
+    breed: "Siberian Husky",
+    characteristic: "Thick fur and blue eyes",
+    isFavorite: true,
+  },
+  {
+    species: "Dog",
+    name: "Bob",
+    breed: "Beagle",
+    characteristic: "Cheerful and likes to play in the park",
+    isFavorite: false,
+  },
+  {
+    species: "Cat",
+    name: "Luna",
+    breed: "Persian",
+    characteristic: "Elegant and clingy",
+    isFavorite: true,
+  },
+  {
+    species: "Cat",
+    name: "Milo",
+    breed: "British Short Hair",
+    characteristic: "Smart and active",
+    isFavorite: true,
+  },
+  {
+    species: "Fish",
+    name: "Nana",
+    breed: "Koi",
+    characteristic: "Beautiful",
+    isFavorite: false,
+  },
+  {
+    species: "Fish",
+    name: "Goldie",
+    breed: "Goldfish",
+    characteristic: "Has bright color scales",
+    isFavorite: false,
+  },
+];
+
+export default pets;
